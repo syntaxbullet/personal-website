778376394e0e75740c518f0366d98f94c9182ad9
syntaxbullet <syntaxbullet@protonmail.com>
2024-01-20 17:50:49 +0100

    chore: prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/absproxy/5173',
-    server: {
-        port: 5173,
-    },
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080"
+  }
 })
