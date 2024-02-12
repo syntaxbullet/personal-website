91dd04d34ea312adb0dc5e262562c657e03ce942
syntaxbullet <syntaxbullet@protonmail.com>
2024-02-12 15:03:52 +0000

    fix: remove redundant basename configuration of react-router

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,11 @@ import { Resume } from "./routes/Resume";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./css/global.css";
 
-const getBaseName = () => { 
- const e = process.env.NODE_ENV;
- if (e == undefined) return "/"
- if (e == "development") return "/"
- return "/" 
-}
 const router = createBrowserRouter(
   [
     { path: "/", element: <Home /> },
     { path: "/work", element: <Resume /> },
-  ],
-  { basename: getBaseName() }
+  ]
 );
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
